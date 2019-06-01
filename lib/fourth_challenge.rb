3197dc9f4de75eebd9201d091a4c6319caaf15c1
The Natural <philip.smalls86@gmail.com>
2019-06-01 15:17:08 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        {}
       ]
    }, 
    :capulet => {
@@ -14,6 +15,7 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {}
       ]
    }
 }
