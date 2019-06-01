83ac4e3b8d09f777fe08f188ddaf093502778868
The Natural <philip.smalls86@gmail.com>
2019-06-01 15:35:10 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,6 +25,7 @@ do
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
 end
+end
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
