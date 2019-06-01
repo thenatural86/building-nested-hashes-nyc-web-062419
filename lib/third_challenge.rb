1c62c0581eefedb4720af4547b0ac1076f162030
The Natural <philip.smalls86@gmail.com>
2019-06-01 15:07:12 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,14 +3,20 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        name: "Lord Montague",
+        age: "53",
         },
       :matriarch => {
         #your key/value pairs here
+        name: "Lady Montague",
+        age: "54"
         },
       :hero => {
         #your key/value pairs here
+        name: "Romeo",
+        age: "15"
         },
-      :hero_friends => []
+        :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
